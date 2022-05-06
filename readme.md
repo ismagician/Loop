cbfc7b45e1513721412e69c3b32ece7676d96fc7
ismagician <92187317+ismagician@users.noreply.github.com>
2022-05-06 13:28:35 -0500

    Adding readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+<h2> Lo-Fi Beat Loop using Java Threads</h2>
+
+This program uses the next loop as guide to make the beat loop
+![Beat Example](./resources/img.png)
+
--- src/TypeofBeat.java
+++ src/TypeofBeat.java
@@ -3,14 +3,9 @@ public class TypeofBeat {
     public static void main(String[] args) {
         int bpm = 75;
 
-
-        //System.out.println(String.format("%.5f", time));
-        //System.out.println(String.format("%.5f", time1));
-
         Piano hithat = new Piano("./sound/piano.wav");
         Drums drums = new Drums("./sound/drums.wav");
         Vocals vocals = new Vocals("./sound/vocals2.wav");
-        //Kick kick = new Kick("./sound/kick.wav", (long)time1);
 
         hithat.start();
         drums.start();
